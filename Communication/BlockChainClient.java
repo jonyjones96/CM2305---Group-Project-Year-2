5d97ca643a8ed25ea8f55fea881871573465adcd
edtrist <edtrist@users.noreply.github.com>
2016-04-16 23:50:41 +0100

    client swapping done - now needs to be integrated with rest of code

--- Communication/BlockChainClient.java
+++ Communication/BlockChainClient.java
@@ -2,7 +2,7 @@ import java.net.*;
 import java.io.*;
 import java.util.*;
 
-public class BlockChainClient {
+public class BlockChainClient extends Thread {
   private BlockChainConnection connection;
   protected static ArrayList<BlockChainConnection> servers = new ArrayList<BlockChainConnection>();
 
@@ -14,12 +14,34 @@ public class BlockChainClient {
 
       connection = new BlockChainConnection(ipAddress, 4567, clientSocket);
       servers.add(connection);
+      start();
+    }
+    catch(Exception e) {
+      System.out.println("Couldn't connect to " + ipAddress);
+    }
+  }
 
-      PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-      BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+  public void run() {
+    try {
+      PrintWriter out = new PrintWriter(connection.getClientSocket().getOutputStream(), true);
+      BufferedReader in = new BufferedReader(new InputStreamReader(connection.getClientSocket().getInputStream()));
+
+      String inLine;
+      while((inLine = in.readLine()) != null) {
+        String messageType = inLine.split(":")[0];
+        String messageContent = inLine.split(":")[1];
+
+        switch(messageType) {
+          case "CLIENT":
+          if(!BlockChainClient.isConnected(messageContent)) {
+            BlockChainClient client = new BlockChainClient(messageContent);
+          }
+          break;
+        }
+      }
     }
     catch(IOException e) {
-      System.out.println("Couldn't connect to " + ipAddress);
+      System.out.println("Couldn't connect to " + connection.getIpAddress());
     }
   }
 
@@ -32,9 +54,5 @@ public class BlockChainClient {
 
   public static void main(String[] args) throws IOException {
     BlockChainClient client = new BlockChainClient("127.0.0.1");
-
-    while(true) {
-      // Keep connection alive
-    }
   }
 }
