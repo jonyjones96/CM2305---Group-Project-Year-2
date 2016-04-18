6a98b4aeff11a867f35f07248cd919be222d4238
Willleung <waiyinleung1996@hotmail.com>
2016-04-18 13:32:38 +0100

    Update Start.java

--- Start.java
+++ Start.java
@@ -1,4 +1,9 @@
+package groupProject;
+
 import java.util.Scanner;
+import java.sql.*;
+import java.util.Date;
+import java.util.Random;
 // import java.RSA.*;
 public class Start{
 
@@ -33,7 +38,7 @@ public class Start{
     				System.out.println("Connected");
 				}
 				Statement stat = con.createStatement();
-				String SQL = "SELECT value FROM dns WHERE `key` = key;";
+				String SQL = "SELECT value FROM dns WHERE `key` =" + key;
 				ResultSet rs;
 				rs = stat.executeQuery(SQL); /*WHERE 'key'='abc123'*/
 		
@@ -88,4 +93,21 @@ public class Start{
 		}	
 		
 	}
+	
+	private static Random random = new Random((new Date()).getTime());
+	
+	public static String generateRandomString(int length) {
+	      char[] values = {'a','b','c','d','e','f','g','h','i','j',
+	               'k','l','m','n','o','p','q','r','s','t',
+	               'u','v','w','x','y','z','0','1','2','3',
+	               '4','5','6','7','8','9'};
+
+	      String out = "";
+
+	      for (int i=0;i<length;i++) {
+	          int idx=random.nextInt(values.length);
+	          out += values[idx];
+	      }
+	      return out;
+	    }
 }
