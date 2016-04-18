29c25a05d692e0d557f0ae98099a15ada77bef3d
jonyjones96 <c1443117@cardiff.ac.uk>
2016-04-18 14:56:04 +0100

    Create testPublicString.java

---
+++ RSA/testPublicString.java
@@ -0,0 +1,62 @@
+import java.io.*;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class testPublicString {
+	
+	public static void main(String[] args) {	
+		// FileReader reader = null;
+		 FileWriter writer = null;
+		//  byte[] test =null;
+		// try {
+		//     reader = new FileReader("public.key");
+		//     // writer = new FileWriter("test10.key");
+		//     int a = 0;
+		//         while ((a = reader.read()) != -1) {
+		//              // writer.write(a);
+		//             // System.out.println(a);
+		//              test = test + a.getBytes();
+		//         }
+		//         // System.out.println(test);
+		//         // writer.write(test);
+
+		//     } catch (FileNotFoundException e) {
+		//     e.printStackTrace();
+		// } catch (IOException e) {
+		//     e.printStackTrace();
+		// } finally {
+		//     try {
+		//         reader.close();
+		//         writer.close();
+		//     } catch (IOException e) {
+		//         e.printStackTrace();
+		//     }
+		// }
+	
+		 BufferedReader reader = null;
+		String total = "";
+		try {
+			writer = new FileWriter("test12.key");
+		    File file = new File("public.key");
+		    reader = new BufferedReader(new FileReader(file));
+
+		    String line;
+		    while ((line = reader.readLine()) != null) {
+		        total = total +line ;
+		        
+		    }
+		    System.out.println(total);
+	writer.write(total);
+		} catch (IOException e) {
+		    e.printStackTrace();
+		} finally {
+		    try {
+		        reader.close();
+		        writer.close();
+		    } catch (IOException e) {
+		        e.printStackTrace();
+		    }
+		}
+	 }
+
+}
