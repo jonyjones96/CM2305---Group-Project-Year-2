7b0525b257b30c72e850d5037fcef293c4aad4d3
Jonathan Jones <jonyjones96@gmail.com>
2016-04-25 11:48:13 +0100

    start should work

--- RSA/randomSeed.java
+++ RSA/randomSeed.java
@@ -1,20 +1,27 @@
-
-
 // import java.util.Date;
 import java.util.Random;
 
-public class RandomGenerator {
+public class randomSeed{
 
-  
+	public static void main(String[] args){
 
-    public static String generateRandomString(int length, int seed) {
+		Random rand = new Random();
+		int randomNum = rand.nextInt(100) ;
+		System.out.println(randomNum);
+		String message = generateRandomString(10,0);
+		System.out.println(message);
+
+	}
+
+
+	public static String generateRandomString(int length, int seed) {
       char[] values = {'a','b','c','d','e','f','g','h','i','j',
                'k','l','m','n','o','p','q','r','s','t',
                'u','v','w','x','y','z','0','1','2','3',
                '4','5','6','7','8','9'};
 
       String out = "";
-      Random random = new Random(15);
+      Random random = new Random(seed);
 
       for (int i=0;i<length;i++) {
           int idx=random.nextInt(values.length);
@@ -22,12 +29,4 @@ public class RandomGenerator {
       }
       return out;
     }
-
-    // public static void main(String[] args){
-    // 	String test = generateRandomString(10);
-    // 	System.out.println("String " + test);
-    	
-
-    // }
 }
-
--- Start.java
+++ Start.java
@@ -1,3 +1,5 @@
+package groupProject;
+
 import java.util.Scanner;
 import java.sql.*;
 import java.util.Date;
@@ -40,7 +42,7 @@ public class Start{
     				//System.out.println("Connected");
 				}
 				Statement stat = con.createStatement();
-				String SQL = "SELECT a_value FROM someBlockChain WHERE `a_key` = '" + key + "'";
+				String SQL = "SELECT a_value FROM BlockChain WHERE `a_key` = '" + key + "'";
 				ResultSet rs;
 				rs = stat.executeQuery(SQL); /*WHERE 'key'='abc123'*/
 				String value = " ";
@@ -100,94 +102,53 @@ public class Start{
 		}
 
 		else if(option.equals("c" )){
-			System.out.println("Enter the name of the Domain you want to change: ");
-			String key = user_input.next( );
-			System.out.println("Enter the location of the private key: ");
-			// Check weather they have the right to update the key
 			try{
-			RSA transferKey = new RSA();
-			String privateKey = user_input.next( );	
-			
-			
-			try {
-				Class.forName("com.mysql.jdbc.Driver");
-			}
-			catch (ClassNotFoundException err) {
-				System.out.println(err);
-			}
-		
-			Connection con = null;
-			try{
-				//my localhost:
-				//con = DriverManager.getConnection("jdbc:mysql://localhost:3306/gproject","root","Willtheshiba12");
-				con = DriverManager.getConnection("jdbc:mysql://csmysql.cs.cf.ac.uk/c1314249","c1314249","rodim8");
-				if (con != null) {
-    				//System.out.println("Connected");
-				}
-				Statement stat = con.createStatement();
-				String SQL = "SELECT publicKey FROM someBlockChain WHERE `a_key` = '" + key + "'";
-				ResultSet rs;
-				rs = stat.executeQuery(SQL); /*WHERE 'key'='abc123'*/
-				String pu = "";
-				while (rs.next()){
-					pu = rs.getString("publicKey");
-					//System.out.println(pu);
-				}
-				FileWriter writer = null;
-				writer = new FileWriter("public.key");
-				writer.write(pu);
-				writer.close();
-				
-				String randomMessage =  generateRandomString(10);
-				//System.out.println(randomMessage);
-				String publicKeyLocation = "public.key";
-				byte[] encypted = transferKey.encryptMessage(publicKeyLocation,randomMessage);
-				String decryptedMessage = new String(transferKey.decryptMessage(privateKey,encypted));
-				if(randomMessage.equals(decryptedMessage)){
+				System.out.println("Enter the name of the Domain you want to change: ");
+				String key = user_input.next( );
+				System.out.println("Enter the location of the private key: ");
+				// Check whether they have the right to update the key
+				RSA transferKey = new RSA();
+				String privateKey = user_input.next( );
+				//System.out.println(privateKey);
+				createTable table = new createTable();
+				int seed = table.getSeed(key);
+				System.out.println("seed: " +seed);
+				String originalMessage = generateRandomString(10,seed);
+				System.out.println("originalMessage: " + originalMessage);
+				String message = table.getEncryptedMessage(key);
+				//byte[] message2 = message.getBytes();
+				System.out.println("encrypted: "+ message);
+				byte[] messageText = transferKey.decryptMessage(privateKey,message);
+				String decryptedMessage = new String(messageText);
+				//Function to get originalText
+				if(decryptedMessage.equals(originalMessage)){
 					// update the database
-					System.out.println("Enter the value: ");
-					String value = user_input.next( );
-					SQL = "Update someBlockChain SET `a_value` = '" + value + "' WHERE `a_key` = '" + key + "'";
-					stat.executeUpdate(SQL);
-					
-					
-					// update evryones database
-				}	
-			
-				con.close();
+					table.updateDomain(key);
+					System.out.println("Your domain has been changed");
+										
+					// update everyones database
+				
+				}
 			}
-			catch (SQLException e){
-				System.out.println(e.getMessage());
-			}
-			// Search the key in the database
-			// String pu = "keyFiles/test3.key";
-			// String pr = "keyFiles/test4.key";
-			// test1.generateKeys(pu,pr);
-			// String randomMessage =  generateRandomString(10);
-			// byte[] encypted =transferKey.encryptMessage(publicKeyLocation,randomMessage);
-			// transferKey.decryptMessage(pr,encypted);
-			
+			catch(Exception e){e.printStackTrace();}
 		}
-		catch(Exception e){}
-		}	
-		
 	}
 	
 	public static String generateRandomString(int length, int seed) {
-      char[] values = {'a','b','c','d','e','f','g','h','i','j',
-               'k','l','m','n','o','p','q','r','s','t',
-               'u','v','w','x','y','z','0','1','2','3',
-               '4','5','6','7','8','9'};
+	      char[] values = {'a','b','c','d','e','f','g','h','i','j',
+	               'k','l','m','n','o','p','q','r','s','t',
+	               'u','v','w','x','y','z','0','1','2','3',
+	               '4','5','6','7','8','9'};
 
-      String out = "";
-      Random random = new Random(seed);
+	      String out = "";
+	      Random random = new Random(seed);
 
-      for (int i=0;i<length;i++) {
-          int idx=random.nextInt(values.length);
-          out += values[idx];
-      }
-      return out;
-    }
+	      for (int i=0;i<length;i++) {
+	          int idx=random.nextInt(values.length);
+	          out += values[idx];
+	      }
+	      return out;
+	      }
 
 	  public static String getPublicKeyString(){
 	  	// FileWriter writer = null;
